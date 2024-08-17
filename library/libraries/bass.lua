f240df5c9cce7ea0d7a1f90d38d94d74c5293275
Periapsises <94928308+Periapsises@users.noreply.github.com>
2024-08-17 04:46:58 +0000

    [Automatic] Update documentation

--- library/libraries/bass.lua
+++ library/libraries/bass.lua
@@ -8,7 +8,7 @@ bass = {}
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- Loads a sound as a Bass object from a file.
 --- 2D sounds require a HUD connection.
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/bass.lua#L191).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/bass.lua#L194).
 ---@param path string # File path to play from.
 ---@param flags string # Flags for the sound (`3d`, `mono`, `noplay`, `noblock`).
 ---@param callback function # Function which is called when the sound is loaded. It'll get 3 arguments: `Bass` object, error number and name.
@@ -17,7 +17,7 @@ function bass.loadFile(path, flags, callback) end
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- Loads a sound as a Bass object from a URL.
 --- 2D sounds require a HUD connection.
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/bass.lua#L209).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/bass.lua#L212).
 ---@param path string # URL path to play from.
 ---@param flags string # Flags for the sound (`3d`, `mono`, `noplay`, `noblock`). noblock will fail if the webserver doesn't provide file length.
 ---@param callback function # Function which is called when the sound is loaded. It'll get 3 arguments: `Bass` object, error number and name.
@@ -25,6 +25,6 @@ function bass.loadURL(path, flags, callback) end
 
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- Returns the number of sounds left that can be created.
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/bass.lua#L227).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/bass.lua#L230).
 ---@return number # The number of sounds left.
 function bass.soundsLeft() end
