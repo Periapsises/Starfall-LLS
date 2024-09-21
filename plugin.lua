6fb9b5b7cd4d39cb8c5f961b905d74a15c0d2648
Periapsises <94928308+Periapsises@users.noreply.github.com>
2024-09-21 16:40:17 -0400

    Add plugin to handle require paths

---
+++ plugin.lua
@@ -0,0 +1,25 @@
+local fs = require 'bee.filesystem'
+local furi = require 'file-uri'
+
+local function findFileInSubfolders( path, name )
+    for file in fs.pairs(path) do
+        if fs.is_directory(file) then
+            local foundPath = findFileInSubfolders(file, name)
+            if foundPath then return foundPath end
+        elseif file:filename():string() == name then
+            return file:string()
+        end
+    end
+end
+
+---@param uri string
+---@param name string
+---@return string[]?
+function ResolveRequire(uri, name)
+    local workspace = fs.path(furi.decode(uri))
+    local path = findFileInSubfolders(workspace, name)
+
+    if path then
+        return { furi.encode(path) }
+    end
+end
