2b958e0262cf7596f6257bf23708f2f6ebcf283c
Periapsises <94928308+Periapsises@users.noreply.github.com>
2026-02-10 15:22:18 -0500

    Add new suri paramter to ResolveRequire

--- plugin.lua
+++ plugin.lua
@@ -1,27 +1,41 @@
 local fs = require("bee.filesystem")
 local furi = require("file-uri")
 
-local function findFileInSubfolders(path, name)
-	for file in fs.pairs(path) do
-		if fs.is_directory(file) then
-			local foundPath = findFileInSubfolders(file, name)
-			if foundPath then
-				return foundPath
-			end
-		elseif file:filename():string() == name then
-			return file:string()
-		end
-	end
-end
-
 ---@param uri string
 ---@param name string
+---@param suri string
 ---@return string[]?
-function ResolveRequire(uri, name)
-	local workspace = fs.path(furi.decode(uri))
-	local path = findFileInSubfolders(workspace, name)
+function ResolveRequire(uri, name, suri)
+	if not suri then return end
 
-	if path then
-		return { furi.encode(path) }
-	end
+    local sourcePath = fs.path(furi.decode(suri))
+
+    local relativePath = sourcePath:parent_path() / name
+    if fs.exists(relativePath) and not fs.is_directory(relativePath) then
+        return { furi.encode(relativePath:string()) }
+    end
+
+    local starfallRoot = sourcePath
+    local foundRoot = false
+
+    while true do
+        if starfallRoot:filename():string() == "starfall" then
+            foundRoot = true
+            break
+        end
+
+        local parent = starfallRoot:parent_path()
+        if not parent or parent == starfallRoot then
+            break
+        end
+
+        starfallRoot = parent
+    end
+
+    if not foundRoot then return end
+
+    local absolutePath = starfallRoot / name
+    if fs.exists(absolutePath) and not fs.is_directory(absolutePath) then
+        return { furi.encode(absolutePath:string()) }
+    end
 end
